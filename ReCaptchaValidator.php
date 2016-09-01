44e9aaedf2a712a6c79fd1cc999e41b3e6656c00
toxor <istvan.penzes@nexum.hu>
2016-09-01 09:15:25 +0200

    use google package

--- ReCaptchaValidator.php
+++ ReCaptchaValidator.php
@@ -73,7 +73,7 @@ class ReCaptchaValidator extends Validator
     /**
      * @param string $value
      * @return array|null
-     * @throws Exception
+     * @throws \Exception
      */
     protected function validateValue($value)
     {
@@ -83,129 +83,15 @@ class ReCaptchaValidator extends Validator
             }
         }
 
-        $request = self::SITE_VERIFY_URL . '?' . http_build_query([
-                'secret' => $this->secret,
-                'response' => $value,
-                'remoteip' => Yii::$app->request->userIP
-            ]);
-        $response = $this->getResponse($request);
-        if (!isset($response['success'])) {
-            throw new Exception('Invalid recaptcha verify response.');
-        }
-        return $response['success'] ? null : [$this->message, []];
-    }
-
-    /**
-     * @param string $request
-     * @return mixed
-     * @throws Exception
-     */
-    protected function getResponse($request)
-    {
-        if ($this->grabberType === self::GRABBER_PHP) {
-            $response = @file_get_contents($request);
-
-            if ($response === false) {
-                throw new Exception('Unable connection to the captcha server.');
-            }
-        } else {
-            $ch = curl_init($request);
-
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-            curl_setopt($ch, CURLOPT_POST, false);
-            curl_setopt($ch, CURLOPT_HEADER, false);
-            curl_setopt($ch, CURLOPT_ENCODING, '');
-            curl_setopt($ch, CURLOPT_AUTOREFERER, true);
-            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 120);
-            curl_setopt($ch, CURLOPT_TIMEOUT, 120);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-            $content = $this->curlExec($ch);
-            $err = curl_errno($ch);
-            $errmsg = curl_error($ch);
-            $header = curl_getinfo($ch);
-            curl_close($ch);
-
-            $header['errno'] = $err;
-            $header['errmsg'] = $errmsg;
-            $response = $content;
-
-            if ($header['errno'] !== 0) {
-                throw new Exception('Unable connection to the captcha server. ' . $header['errmsg']);
-            }
+        try {
+            $recaptcha = new \ReCaptcha\ReCaptcha($this->secret);
+            $response  = $recaptcha->verify($value, Yii::$app->request->userIP);
+            $success   = $response->isSuccess();
+        } catch (\Exception $e) {
+            throw $e;
+            $success   = false;
         }
 
-        return Json::decode($response, true);
-    }
-
-    protected function curlExec($ch, &$maxredirect = null)
-    {
-        // we emulate a browser here since some websites detect
-        // us as a bot and don't let us do our job
-        $user_agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7.5)" .
-            " Gecko/20041107 Firefox/1.0";
-        curl_setopt($ch, CURLOPT_USERAGENT, $user_agent);
-
-        $mr = $maxredirect === null ? 10 : intval($maxredirect);
-
-        if (
-            filter_var(ini_get('open_basedir'), FILTER_VALIDATE_BOOLEAN) === false &&
-            filter_var(ini_get('safe_mode'), FILTER_VALIDATE_BOOLEAN) === false
-        ) {
-            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, $mr > 0);
-            curl_setopt($ch, CURLOPT_MAXREDIRS, $mr);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        } else {
-
-            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
-
-            if ($mr > 0) {
-                $original_url = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
-                $newurl = $original_url;
-
-                $rch = curl_copy_handle($ch);
-
-                curl_setopt($rch, CURLOPT_HEADER, true);
-                curl_setopt($rch, CURLOPT_NOBODY, true);
-                curl_setopt($rch, CURLOPT_FORBID_REUSE, false);
-
-                do {
-                    curl_setopt($rch, CURLOPT_URL, $newurl);
-                    $header = curl_exec($rch);
-                    if (curl_errno($rch)) {
-                        $code = 0;
-                    } else {
-                        $code = curl_getinfo($rch, CURLINFO_HTTP_CODE);
-                        if ($code == 301 || $code == 302) {
-                            preg_match('/Location:(.*?)\n/i', $header, $matches);
-                            $newurl = trim(array_pop($matches));
-
-                            // if no scheme is present then the new url is a
-                            // relative path and thus needs some extra care
-                            if (!preg_match("/^https?:/i", $newurl)) {
-                                $newurl = $original_url . $newurl;
-                            }
-                        } else {
-                            $code = 0;
-                        }
-                    }
-                } while ($code && --$mr);
-
-                curl_close($rch);
-
-                if (!$mr) {
-                    if ($maxredirect === null)
-                        trigger_error('Too many redirects.', E_USER_WARNING);
-                    else
-                        $maxredirect = 0;
-
-                    return false;
-                }
-                curl_setopt($ch, CURLOPT_URL, $newurl);
-            }
-        }
-
-        return curl_exec($ch);
+        return $success ? null : [$this->message, []];
     }
 }
